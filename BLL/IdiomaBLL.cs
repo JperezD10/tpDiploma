1e46a91e471b6a3badedd63901c6dc4aa76a64d4
jperezdemonty <jperezdemonty@quaresitsolutions.com>
2021-11-12 13:43:02 -0300

    Bitacora para idioma

--- BLL/IdiomaBLL.cs
+++ BLL/IdiomaBLL.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using DAL;
 using BE;
+using SEGURIDAD;
 
 namespace BLL
 {
     public class IdiomaBLL
     {
         IdiomaDAL mapper = new IdiomaDAL();
+        Encriptacion encriptacion = new Encriptacion();
+        BitacoraBLL ServicioBitacora = new BitacoraBLL();
+        Usuario_Sesion Usuario_Sesion = Usuario_Sesion.Instance;
         public List<string> CargarIdiomas()
         {
             return mapper.CargarIdiomas();
@@ -29,11 +33,27 @@ namespace BLL
         }
         public void GuardarTraduccion(IdiomaControl control, string idioma)
         {
+            Bitacora bitacora = new Bitacora()
+            {
+                Accion = "Nueva traduccion",
+                Criticidad = "Medio",
+                Descripcion = encriptacion.encriptar($"Se ha traducido el control {control.NombreControl}"),
+                Usuario = encriptacion.encriptar(Usuario_Sesion.Username)
+            };
+            ServicioBitacora.crearBitacora(bitacora);
             mapper.GuardarTraduccion(control, idioma);
         }
         public void GuardarIdioma(string idioma)
         {
             mapper.GuardarIdioma(idioma);
+            Bitacora bitacora = new Bitacora()
+            {
+                Accion = "Nuevo idioma",
+                Criticidad = "Alta",
+                Descripcion = encriptacion.encriptar("Se ha registrado un nuevo idioma"),
+                Usuario = encriptacion.encriptar(Usuario_Sesion.Username)
+            };
+            ServicioBitacora.crearBitacora(bitacora);
         }
     }
 }
