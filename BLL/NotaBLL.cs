bd23b063a08c0719b91360cafa8d486680c87588
Julian Perez Demonty <jperezdemonty@live.com.ar>
2021-11-21 00:04:32 -0300

    nota backend hecho

---
+++ BLL/NotaBLL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using DAL;
+
+namespace BLL
+{
+    public class NotaBLL
+    {
+        NotaDAL mapper = new NotaDAL();
+        public bool ValidarNotaDisponibleParaTrimestre(Nota nota, int idAlumno, int trimestre)
+        {
+            return mapper.ValidarNotaDisponibleParaTrimestre(nota, idAlumno, trimestre);
+        }
+
+        public void RegistrarNotaPorTrimestre(Nota nota, int id_Alumno, int trimestre)
+        {
+            mapper.RegistrarNotasPorTrimestre(nota,id_Alumno, trimestre);
+        }
+    }
+}
--- DAL/NotaDAL.cs
+++ DAL/NotaDAL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,38 @@ namespace DAL
                 };
                 acceso.Escribir("RegistrarNotasHistoricas", parametros);
             }
+        }
+        public void RegistrarNotasPorTrimestre(Nota nota, int id_Alumno, int trimestre)
+        {
+            SqlParameter[] parametros =
+            {
+                new SqlParameter("@idAlumno",id_Alumno),
+                new SqlParameter("@idMateria",nota.Materia.ID_Materia),
+                new SqlParameter("@notaNumerica",nota.NotaNumerica),
+                new SqlParameter("@trimestre",trimestre)
+            };
+            acceso.Escribir("PonerNotaTrimestral", parametros);
+        }
+
+        public bool ValidarNotaDisponibleParaTrimestre(Nota nota, int id_Alumno, int trimestre)
+        {
+            bool salida = true;
+            SqlParameter[] parametros =
+            {
+                new SqlParameter("@idAlumno",id_Alumno),
+                new SqlParameter("@idMateria",nota.Materia.ID_Materia),
+                new SqlParameter("@trimestre",trimestre)
+            };
+            DataTable resultado = acceso.Leer("ValidarNotaDisponibleParaTrimestre", parametros);
+            if (resultado.Rows.Count>0)
+            {
+                salida = false;
+            }
+            else
+            {
+                salida = true;
+            }
+            return salida;
         }
     }
 }
